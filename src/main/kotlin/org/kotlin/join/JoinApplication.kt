dddecec19f3b3a42e26ae668964d38e5a49e0d97
Griotold <gotjd9773@naver.com>
2024-07-05 10:39:01 +0900

    Initial commit

---
+++ src/main/kotlin/org/kotlin/join/JoinApplication.kt
@@ -0,0 +1,11 @@
+package org.kotlin.join
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JoinApplication
+
+fun main(args: Array<String>) {
+    runApplication<JoinApplication>(*args)
+}
---
+++ src/test/kotlin/org/kotlin/join/JoinApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.kotlin.join
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JoinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
